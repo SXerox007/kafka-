de6ec12df8d2f067d2835810ce5867abfa04873c
SUMIT_THAKUR <sumit.thakur@clicklabs.co>
2019-02-11 23:26:18 +0530

    producers create and send record done

--- src/main/java/com/github/SXerox007/kafka_java_intro/Producer.java
+++ src/main/java/com/github/SXerox007/kafka_java_intro/Producer.java
@@ -1,8 +1,46 @@
 package com.github.SXerox007.kafka_java_intro;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+
+class ProducersElements{
+
+    //create properties
+    private Properties createProperties(){
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,constants.BOOTSTRAP_SERVER);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+
+    // Create the producers
+    public KafkaProducer<String,String> createProducers(){
+        return new KafkaProducer<String, String>(createProperties());
+    }
+
+    public ProducerRecord<String, String> createRecord(){
+        return new ProducerRecord<String, String>("first_topic","hello");
+    }
+}
+
+
+//Producer Kafka
 public class Producer {
 
     public static void main(String[] args) {
-
+        ProducersElements producersElements = new ProducersElements();
+        //data send is async
+        KafkaProducer<String, String> producer = producersElements.createProducers();
+        producer.send(producersElements.createRecord());
+        //flush and close
+        producer.flush();
+        producer.close();
     }
+
 }
---
+++ src/main/java/com/github/SXerox007/kafka_java_intro/constants.java
@@ -0,0 +1,5 @@
+package com.github.SXerox007.kafka_java_intro;
+
+public interface constants {
+    String BOOTSTRAP_SERVER = "localhost:9092";
+}
