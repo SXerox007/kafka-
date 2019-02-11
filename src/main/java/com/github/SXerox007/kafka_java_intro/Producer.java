d950d0f22a00dd711a9a2c3302f288cb4f0e6ba5
SUMIT_THAKUR <sumit.thakur@clicklabs.co>
2019-02-12 00:34:44 +0530

    add logs and check with the consumer success done

--- src/main/java/com/github/SXerox007/kafka_java_intro/Producer.java
+++ src/main/java/com/github/SXerox007/kafka_java_intro/Producer.java
@@ -12,6 +12,7 @@ class ProducersElements{
 
     //create properties
     private Properties createProperties(){
+        System.out.println("Create Properties");
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,constants.BOOTSTRAP_SERVER);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
@@ -21,11 +22,13 @@ class ProducersElements{
 
     // Create the producers
     public KafkaProducer<String,String> createProducers(){
+        System.out.println("Create Producers");
         return new KafkaProducer<String, String>(createProperties());
     }
 
     public ProducerRecord<String, String> createRecord(){
-        return new ProducerRecord<String, String>("first_topic","hello");
+        System.out.println("Create Record");
+        return new ProducerRecord<String, String>("first_topic","hello sumit");
     }
 }
 
--- src/main/java/com/github/SXerox007/kafka_java_intro/info.sh
+++ src/main/java/com/github/SXerox007/kafka_java_intro/info.sh
@@ -1,1 +1,4 @@
 # https://kafka.apache.org/documentation/#producerconfig
+
+# Test the basic consumer hello sumit
+kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first_topic --group first_group
