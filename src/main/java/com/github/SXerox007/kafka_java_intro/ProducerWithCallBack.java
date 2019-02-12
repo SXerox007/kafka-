bb6fba1a7c72aa6cc32b7d3e1129c546e6206169
SUMIT_THAKUR <sumit.thakur@clicklabs.co>
2019-02-12 11:56:33 +0530

    producer with callback done

--- src/main/java/com/github/SXerox007/kafka_java_intro/ProducerWithCallBack.java
+++ src/main/java/com/github/SXerox007/kafka_java_intro/ProducerWithCallBack.java
@@ -1,18 +1,18 @@
 package com.github.SXerox007.kafka_java_intro;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 
-class ProducersElements{
+class ProducerCallBackElements {
+
 
     //create properties
     private Properties createProperties(){
-        System.out.println("Create Properties");
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,constants.BOOTSTRAP_SERVER);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
@@ -22,25 +22,41 @@ class ProducersElements{
 
     // Create the producers
     public KafkaProducer<String,String> createProducers(){
-        System.out.println("Create Producers");
         return new KafkaProducer<String, String>(createProperties());
     }
 
     public ProducerRecord<String, String> createRecord(){
-        System.out.println("Create Record");
         return new ProducerRecord<String, String>("first_topic","hello sumit");
     }
 }
 
 
 //Producer Kafka
-public class Producer {
+public class ProducerWithCallBack {
 
     public static void main(String[] args) {
+        final Logger logger = LoggerFactory.getLogger(ProducerWithCallBack.class);
         ProducerCallBackElements producersElements = new ProducerCallBackElements();
         //data send is async
-        KafkaProducer<String, String> producer = producersElements.createProducers();
-        producer.send(producersElements.createRecord());
+        final KafkaProducer<String, String> producer = producersElements.createProducers();
+        //send msg with callback
+        //callback tells us is there any exception or success
+        producer.send(producersElements.createRecord(), new Callback() {
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                if (e == null){
+                    //success
+                    logger.info("Topic: " +  recordMetadata.topic() +
+                    "\n OffSet: " + recordMetadata.hasOffset() +
+                    "\n TimeStamp: " +  recordMetadata.hasTimestamp() +
+                    "\n Partition: " + recordMetadata.partition());
+
+                }else{
+                    //error
+                    logger.error("Error while sending the data to topic: " +  e.getMessage());
+                }
+
+            }
+        });
         //flush and close
         producer.flush();
         producer.close();
