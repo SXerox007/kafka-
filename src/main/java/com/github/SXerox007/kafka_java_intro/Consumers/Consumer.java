215beee9a45381ed15dba1bde56a89be72e23c4f
SUMIT_THAKUR <sumit.thakur@clicklabs.co>
2019-02-13 13:10:59 +0530

    make basic consumer with test

--- src/main/java/com/github/SXerox007/kafka_java_intro/Consumers/Consumer.java
+++ src/main/java/com/github/SXerox007/kafka_java_intro/Consumers/Consumer.java
@@ -2,11 +2,14 @@ package com.github.SXerox007.kafka_java_intro.Consumers;
 
 import com.github.SXerox007.kafka_java_intro.Constants.constants;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Properties;
@@ -19,7 +22,7 @@ class ConsumerElements{
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, constants.BOOTSTRAP_SERVER);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,constants.GROUP_ID);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,constants.OFFSET_LATEST);
         return properties;
@@ -27,7 +30,7 @@ class ConsumerElements{
 
     // Create the consumers
     public KafkaConsumer<String,String> createConsumer(){
-        return new KafkaConsumer<String, String>(createProperties());
+        return new KafkaConsumer<>(createProperties());
     }
 
     //subscribe single topic only
@@ -48,7 +51,18 @@ public class Consumer {
 
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(Consumer.class.getName());
+        ConsumerElements consumerElements = new ConsumerElements();
+        KafkaConsumer<String,String> consumer = consumerElements.createConsumer();
+        consumer.subscribe(Arrays.asList(constants.TOPIC_NAME));
 
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("\nKey: " + record.key() + " Value: " + record.value());
+                logger.info("\nPartition: " + record.partition() + " Offset: " + record.offset());
+            }
+        }
     }
 
 }
