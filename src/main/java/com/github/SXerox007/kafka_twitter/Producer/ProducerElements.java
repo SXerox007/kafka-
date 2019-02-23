2206b6a3fb33a634bfae63a33cc707a44e9b8ebf
SUMIT_THAKUR <sumit.thakur@clicklabs.co>
2019-02-24 02:58:39 +0530

    add properties for high throughput

--- src/main/java/com/github/SXerox007/kafka_twitter/Producer/ProducerElements.java
+++ src/main/java/com/github/SXerox007/kafka_twitter/Producer/ProducerElements.java
@@ -23,6 +23,13 @@ public class ProducerElements {
         properties.setProperty(ProducerConfig.RETRIES_CONFIG,Integer.toString(10)); // i will do the retries msg to 10 time then ignore the msg
         // We can set the Retries config to Integer.MAX_VALUE
         properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION,"5"); // multiple requests connections
+
+
+        //high throughput producer bit of latency and CPU usage
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG,"snappy"); //we us data compression for snappy
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG,"20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG,Integer.toString(32*1024));
+
         return properties;
     }
 
